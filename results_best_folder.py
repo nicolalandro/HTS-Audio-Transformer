07fba5939355a687e80e1abaeb42b10528833858
artelabsuper <artelab@gmail.com>
2022-08-09 09:38:05 +0200

    experiments

--- results_best_folder.py
+++ results_best_folder.py
@@ -1,7 +1,7 @@
 from audioop import avg
 import os
 
-folder = "logs/urbansound_scratch"
+folder = "logs/urbansound_audioset"
 best_accs = []
 for f in os.listdir(folder):
     file_path = os.path.join(folder, f)
