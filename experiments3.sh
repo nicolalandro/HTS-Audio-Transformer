29a4916459bbbf55252d7219082b64c563f422d6
artelabsuper <artelab@gmail.com>
2022-08-10 12:15:49 +0200

    try 8 and 9

--- experiments3.sh
+++ experiments3.sh
@@ -1,6 +1,6 @@
 source venv/bin/activate
 
-for i in 2
+for i in 8 9
 do
     MASTER_ADDR=localhost MASTER_PORT=29501 FOLD=$i\
     ROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python CUDA_VISIBLE_DEVICES=1 \
